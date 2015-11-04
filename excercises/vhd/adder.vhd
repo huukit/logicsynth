e5da0614670b8c199f8e178ff0ca84c3caab279e
Tuomas Huuki <tuomas.huuki@student.tut.fi>
2015-11-04 10:41:16 +0200

    More comments added.

--- excercises/vhd/adder.vhd
+++ excercises/vhd/adder.vhd
@@ -1,5 +1,5 @@
 -------------------------------------------------------------------------------
--- Title      : TIE-50206, Exercise 02
+-- Title      : TIE-50206, Exercise 03
 -- Project    : 
 -------------------------------------------------------------------------------
 -- File       : adder.vhd
@@ -15,7 +15,7 @@
 -------------------------------------------------------------------------------
 -- Revisions  :
 -- Date        Version  Author  Description
--- 4.11.2015  1.0      tuhu    Created
+-- 4.11.2015   1.0      tuhu    Created
 -------------------------------------------------------------------------------
 library ieee;
 use ieee.std_logic_1164.all;
@@ -23,14 +23,14 @@ use ieee.numeric_std.all;
 
 entity adder is
   generic(
-    operand_width_g : integer
-  );
+    operand_width_g : integer -- Width of input and output. Note, that if the definition of the input width
+  );                          -- is i.e 8, the definition below must be (8 - 1) because the vector starts from 0. 
     
   port(
     clk, rst_n : in std_logic;
-    a_in : in std_logic_vector(operand_width_g - 1 downto 0);
-    b_in : in std_logic_vector(operand_width_g - 1 downto 0);
-    sum_out : out std_logic_vector(operand_width_g downto 0)
+    a_in : in std_logic_vector(operand_width_g - 1 downto 0); -- Input a.
+    b_in : in std_logic_vector(operand_width_g - 1 downto 0); -- Input b.
+    sum_out : out std_logic_vector(operand_width_g downto 0)  -- Sum output.
   );
     
 end adder;
@@ -41,9 +41,8 @@ architecture rtl of adder is
   
 begin -- rtl
   sum_out <= std_logic_vector(result); -- Assign register to output.
-  
-  -- Handle the actual calculation of values.
-  CALCPROC : process (clk, rst_n)
+ 
+  CALCPROC : process (clk, rst_n) -- Handle the actual calculation of values and reset.
   begin
     if(rst_n = '0') then -- Reset
       result <= (others => '0');
