a98df94c324d2827076a18f21feb62eba79278c2
Tuomas Huuki <tuomas.huuki@student.tut.fi>
2015-11-22 10:29:33 +0200

    .gitignore update, also added modified adder.vhd.

--- excercises/vhd/adder.vhd
+++ excercises/vhd/adder.vhd
@@ -3,7 +3,7 @@
 -- Project    : 
 -------------------------------------------------------------------------------
 -- File       : adder.vhd
--- Author     : Tuomas Huuki
+-- Author     : Tuomas Huuki, Jonas Nikula
 -- Company    : TUT
 -- Created    : 4.11.2015
 -- Platform   : 
@@ -14,9 +14,10 @@
 -- Copyright (c) 2015 
 -------------------------------------------------------------------------------
 -- Revisions  :
--- Date        Version  Author  Description
--- 4.11.2015   1.0      tuhu    Created
--- 18.11.2015  1.1      tuhu    Modified process.
+-- Date        Version  Author      Description
+-- 4.11.2015   1.0      tuhu        Created
+-- 18.11.2015  1.1      tuhu        Modified process.
+-- 20.11.2015  1.1      nikulaj     Added bonus feature
 -------------------------------------------------------------------------------
 library ieee;
 use ieee.std_logic_1164.all;
@@ -25,7 +26,8 @@ use ieee.numeric_std.all;
 entity adder is
   generic(
     operand_width_g : integer -- Width of input and output. Note, that if the definition of the input width
-  );                          -- is i.e 8, the definition below must be (8 - 1) because the vector starts from 0. 
+                          -- is i.e 8, the definition below must be (8 - 1) because the vector starts from 0. 
+  );
     
   port(
     clk : in std_logic;                                       -- Clock signal.
@@ -40,10 +42,10 @@ end adder;
 architecture rtl of adder is
   
   SIGNAL result_r : signed((operand_width_g) downto 0); -- Result register.
-  
+
 begin -- rtl
   sum_out <= std_logic_vector(result_r); -- Assign register to output.
- 
+
   calculate : process (clk, rst_n) -- Handle the actual calculation of values and reset.
   begin
     if(rst_n = '0') then -- Reset
