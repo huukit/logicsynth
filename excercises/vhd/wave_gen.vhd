dc59816c954838e231331e7758eeffb7911837e5
Tuomas Huuki <tuomas.huuki@student.tut.fi>
2015-11-25 10:56:28 +0200

    Added comments to implementation.

--- excercises/vhd/wave_gen.vhd
+++ excercises/vhd/wave_gen.vhd
@@ -22,66 +22,67 @@ library ieee;
 use ieee.std_logic_1164.all;
 use ieee.numeric_std.all;
 
-entity wave_gen is
+entity wave_gen is -- Wave generator entity.
   
   generic (
-    width_g             : integer;
-    step_g              : integer
-);
- 
- port(
-   clk                  : in std_logic;
-   rst_n                : in std_logic;
-   sync_clear_in        : in std_logic;
-   value_out            : out std_logic_vector(width_g - 1 downto 0)  
- );
- 
+    width_g             : integer; -- Width of the generated wave in bits.
+    step_g              : integer -- Width of one step.
+  );
+  port(
+    clk                 : in std_logic; -- Clock signal.
+    rst_n               : in std_logic; -- Reset, actove low.
+    sync_clear_in       : in std_logic; -- Sync bit input to clear the counter.
+    value_out           : out std_logic_vector(width_g - 1 downto 0)  -- Counter value out.
+  );
 end wave_gen;
 
 architecture rtl of wave_gen is
-  type dir_type is (up, down);
-  constant maxval_c      : integer := (2**(width_g - 1) - 1)/step_g * step_g;
-  constant minval_c     : integer := -maxval_c;
   
-  signal dir_r          : dir_type;
-  signal count_r        : signed(width_g - 1 downto 0);
+  type dir_type is (up, down); -- Definition for counter direction type.
+    
+  constant maxval_c     : integer := (2**(width_g - 1) - 1)/step_g * step_g; -- Maximum value for counter.
+  constant minval_c     : integer := -maxval_c; -- Minimun value for counter.
+  
+  signal dir_r          : dir_type; -- Counter direction.
+  signal count_r        : signed(width_g - 1 downto 0); -- Counter count register.
   
 begin -- rtl
-  value_out <= std_logic_vector(count_r);
+
+  value_out <= std_logic_vector(count_r); -- Assign register to output.
   
-  count : process (clk, rst_n)
+  count : process (clk, rst_n) -- Process to increment or decrement counter value.
   begin
-    if(rst_n = '0') then
-      count_r <= (others => '0');
-    elsif(clk'event and clk = '1') then
+    if(rst_n = '0') then 
+      count_r <= (others => '0'); -- Clear the output on reset ...
+    elsif(clk'event and clk = '1') then 
       if(sync_clear_in = '1') then
-        count_r <= (others => '0');
+        count_r <= (others => '0'); -- ... or if the sync bit is active.
       else
-        case (dir_r) is
+        case (dir_r) is -- Check the direction and step the counter up or down.
           when up =>
-            count_r <= count_r + step_g;
+            count_r <= count_r + step_g; -- Step counter up.
           when down =>
-            count_r <= count_r - step_g;
-        end case;
-      end if;
-    end if;
+            count_r <= count_r - step_g; -- Step counter down.
+        end case; -- dir_r
+      end if; -- sync_clear_in
+    end if; -- clk'event ...
   end process count;
   
-  compare : process (clk, rst_n)
+  compare : process (clk, rst_n) -- Process to do compare and change direction.
   begin
-    if(rst_n = '0') then
-      dir_r <= up;
+    if(rst_n = '0') then 
+      dir_r <= up; -- Set the default direction of the counter on reset ...
     elsif(clk'event and clk = '1') then
       if(sync_clear_in = '1') then
-        dir_r <= up;
+        dir_r <= up; -- ... and also set the counter direction on sync bit active.
       else
-        if(count_r + step_g = maxval_c) then
-          dir_r <= down;
-        elsif(count_r - step_g = minval_c) then
-          dir_r <= up;
-        end if;
-      end if;
-    end if;
+        if((count_r + step_g) = maxval_c) then     -- If we are on the limit on the next round ...
+          dir_r <= down;                           -- ... change the direction of the counter. 
+        elsif((count_r - step_g) = minval_c) then  -- Note, as assignments take place after the block          
+          dir_r <= up;                             -- has been executed, we need to change the direction 
+        end if; -- count_r                         -- before hitting the actual limit.
+      end if; -- sync_clear_in
+    end if; -- clk'event ...
   end process compare;
   
 end rtl;
