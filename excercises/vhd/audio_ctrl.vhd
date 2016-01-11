ccb6175b8115786eb041123706c469c9f98576a4
nikulaj <aJns@users.noreply.github.com>
2016-01-11 19:34:53 +0000

    Define generics and ports

--- excercises/vhd/audio_ctrl.vhd
+++ excercises/vhd/audio_ctrl.vhd
@@ -25,9 +25,21 @@ use ieee.numeric_std.all;
 entity audio_ctrl is
     generic
     (
+    ref_clk_freq_g  : integer := 18 432 000;
+    sample_rate_g   : integer := 48 000;
+    data_width_g    : integer := 16;
     );
+
     port
     (
+    clk             : in std_logic;
+    rst_n           : in std_logic;
+    left_data_in    : in std_logic_vector(data_width_g - 1 downto 0);
+    right_data_in   : in std_logic_vector(data_width_g - 1 downto 0);
+
+    aud_bclk_out    : out std_logic;
+    aud_data_out    : out std_logic;
+    aud_lrclk_out   : out std_logic;
     );
 end audio_ctrl;
 
