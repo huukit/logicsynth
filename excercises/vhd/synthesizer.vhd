0c37dd6574c6da7990cde675e84f86c0026119d5
nikulaj <aJns@users.noreply.github.com>
2016-01-15 13:36:41 +0200

    Add component declarations to synthesizer.vhd

--- excercises/vhd/synthesizer.vhd
+++ excercises/vhd/synthesizer.vhd
@@ -42,5 +42,53 @@ entity synthesizer is
 end synthesizer;
 
 architecture rtl of synthesizer is
+    component wave_gen is
+        generic
+        (
+            width_g             : integer; -- Width of the generated wave in bits.
+            step_g              : integer -- Width of one step.
+        );
+        port
+        (
+            clk                 : in std_logic; -- Clock signal.
+            rst_n               : in std_logic; -- Reset, actove low.
+            sync_clear_in       : in std_logic; -- Sync bit input to clear the counter.
+            value_out           : out std_logic_vector(width_g - 1 downto 0)  -- Counter value out.
+        );
+    end component;
+
+    component multi_port_adder is
+        generic
+        (
+            operand_width_g     : integer := 16; -- Specify default value for both.
+            num_of_operands_g   : integer := 4
+        );
+        port
+        (
+            clk         : in std_logic; -- Clock signal.
+            rst_n       : in std_logic; -- Reset, active low.
+            operands_in : in std_logic_vector((operand_width_g * num_of_operands_g) - 1 downto 0); -- Operand inputs
+            sum_out     : out std_logic_vector(operand_width_g - 1 downto 0) -- Calculation result.
+        )
+    end component;
+
+    component audio_ctrl is
+        generic
+        (
+            ref_clk_freq_g  : integer := 18432000;  -- Reference clock.
+            sample_rate_g   : integer := 48000;     -- Sample clock fs.
+            data_width_g    : integer := 16         -- Data width.
+        );
+        port
+        (
+            clk             : in std_logic;         -- Main clock.
+            rst_n           : in std_logic;         -- Reset, active low.
+            left_data_in    : in std_logic_vector(data_width_g - 1 downto 0); -- Data in, left.
+            right_data_in   : in std_logic_vector(data_width_g - 1 downto 0); -- Data in, right.
+            aud_bclk_out    : out std_logic;        -- Audio bitclock.
+            aud_data_out    : out std_logic;        -- Audio data.
+            aud_lrclk_out   : out std_logic         -- Audio bitclock L/R select.
+        );
+    end component;
 begin -- rtl
 end rtl;
