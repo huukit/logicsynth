68a77e7666b9c72fb4436d970aae9c71a7dee297
nikulaj <aJns@users.noreply.github.com>
2016-01-24 20:05:50 +0200

    Implement start condition and address transfer

--- excercises/vhd/i2c_config.vhd
+++ excercises/vhd/i2c_config.vhd
@@ -40,28 +40,109 @@ end i2c_config;
 
 architecture rtl of i2c_config is
     
-    constant prescaler_max      : integer := (ref_clk_freq_g / i2c_freq_g) / 2;
+    constant prescaler_max_c    : integer := (ref_clk_freq_g / i2c_freq_g) / 2;
 
-    signal sclk_out_r           : std_logic;
-    signal sclk_out_prescaler   : unsigned(integer(ceil(log2(real(prescaler_max)))) downto 0);
+    signal sclk_r               : std_logic;
+    signal sclk_prescaler_r     : unsigned(integer(ceil(log2(real(prescaler_max_c)))) downto 0);
+
+    signal finished_r           : std_logic := '0';
+
+    signal sdat_r               : std_logic := 'Z';
+
+    type state_type is (start_condition, stop_condition,
+                        acknowledge, address_transfer, data_transfer);
+    signal present_state_r      : state_type;
+
+    signal bit_counter_r        : unsigned(2 downto 0) := (others => '0');
+
+    signal temp_address_r       : std_logic_vector(7 downto 0);
+    signal temp_data_r          : std_logic_vector(7 downto 0);
+    type transmission_arr is array (n_params_g - 1 downto 0) of std_logic_vector(15 downto 0);
+    -- signal data_r               : data_arr;
+    signal transmission_c       : transmission_arr := ( "0000000000011010",
+                                                        "0000001000011010",
+                                                        "0000010001111011",
+                                                        "0000011001111011",
+                                                        "0000100011111000",
+                                                        "0000101000000110",
+                                                        "0000110000000000",
+                                                        "0000111XXXXXXXXX",
+                                                        "0001000XXXXXXXXX",
+                                                        "0001001000000001"  );
+
+    -- data_r(0) <= "0000000000011010";
+    -- data_r(1) <= "0000001000011010";
+    -- data_r(2) <= "0000010001111011";
+    -- data_r(3) <= "0000011001111011";
+    -- data_r(4) <= "0000100011111000";
+    -- data_r(5) <= "0000101000000110";
+    -- data_r(6) <= "0000110000000000";
+    -- data_r(7) <= "0000111XXXXXXXXX";
+    -- data_r(8) <= "0001000XXXXXXXXX";
+    -- data_r(9) <= "0001001000000001";
 
 begin
 
-    sclk_out <= sclk_out_r;
+    sclk_out <= sclk_r;
+    finished_out <= finished_r;
+    sdat_inout <= sdat_r;
 
-    generate_sclk_out : process(clk, rst_n)
+    generate_sclk : process(clk, rst_n)
     begin
         if(rst_n = '0') then
-            sclk_out_r <= '0';
-            sclk_out_prescaler <= (others => '0');
+            sclk_r <= '0';
+            sclk_prescaler_r <= (others => '0');
         elsif(clk'event and clk = '1') then
-            if(sclk_out_prescaler = prescaler_max) then
-                sclk_out_prescaler <= (others => '0');
-                sclk_out_r <= not sclk_out_r;
+            if(sclk_prescaler_r = prescaler_max_c) then
+                sclk_prescaler_r <= (others => '0');
+                sclk_r <= not sclk_r;
             else
-                sclk_out_prescaler <= sclk_out_prescaler + 1;
+                sclk_prescaler_r <= sclk_prescaler_r + 1;
             end if;
         end if;
-    end process generate_sclk_out;
+    end process generate_sclk;
+
+    generate_sdat : process(clk, rst_n)
+    begin
+        if(rst_n = '0') then
+            sdat_r <= 'Z';
+            present_state_r <= start_condition;
+            bit_counter_r <= to_unsigned(7, 3);
+
+        elsif(clk'event and clk = '1') then
+
+            case present_state_r is 
+
+                when start_condition =>
+                    if(sdat_r = '1' and sclk_r = '1' and sclk_prescaler_r = prescaler_max_c / 2) then
+                        sdat_r <= '0';
+                        present_state_r <= address_transfer;
+                        temp_address_r <= transmission_c(0)(15 downto 8);
+                    elsif(sdat_r = '0' or sdat_r = 'Z') then
+                        sdat_r <= '1';
+                    end if;
+
+                when stop_condition =>
+
+                when acknowledge =>
+
+                when address_transfer =>
+                    -- if(sclk_r'event and sclk_r = '0') then
+                    if(sclk_r = '0' and sclk_prescaler_r = 0) then
+                        if(bit_counter_r = 0) then
+                            bit_counter_r <= to_unsigned(7, 3);
+                        end if;
+                        sdat_r <= temp_address_r(to_integer(bit_counter_r));
+                        bit_counter_r <= bit_counter_r - 1;
+                    end if;
+
+
+                when data_transfer =>
+
+            end case;
+
+        end if;
+
+    end process generate_sdat;
 
 end rtl;
