207c2c20e50e90f75692dae8b2057fcc4462931c
nikulaj <aJns@users.noreply.github.com>
2016-01-27 00:03:16 +0200

    Restructure i2c_config
    Changed i2c_config to function more closely to the specs.
    
    Now it sends the required address byte at the beginning. Now it also has
    only one state for sending data.
    
    However, it can only send the first three bytes. For some reason it gets
    stuck at acknowledge.

--- excercises/vhd/i2c_config.vhd
+++ excercises/vhd/i2c_config.vhd
@@ -39,45 +39,44 @@ entity i2c_config is
 end i2c_config;
 
 architecture rtl of i2c_config is
+
+    type state_type is (start_condition, stop_condition, acknowledge, data_transfer);
+    type transmission_data_arr is array (n_params_g - 1 downto 0) of std_logic_vector(15 downto 0);
+    type temp_transmission_arr is array (2 downto 0) of std_logic_vector(7 downto 0);
     
     constant prescaler_max_c    : integer := (ref_clk_freq_g / i2c_freq_g) / 2;
+    constant codec_address_c    : std_logic_vector(7 downto 0) := "00110100";
+    constant transmission_data_c  : transmission_data_arr := (
+                                                            "0001001000000001",
+                                                            "0001000XXXXXXXXX",
+                                                            "0000111XXXXXXXXX",
+                                                            "0000110000000000",
+                                                            "0000101000000110",
+                                                            "0000100011111000",
+                                                            "0000011001111011",
+                                                            "0000010001111011",
+                                                            "0000001000011010",
+                                                            "0000000000011010"
+                                                            );
 
-    signal sclk_r               : std_logic;
-    signal sclk_prescaler_r     : unsigned(integer(ceil(log2(real(prescaler_max_c)))) downto 0);
+    signal sclk_r                   : std_logic;
+    signal sclk_prescaler_r         : unsigned(integer(ceil(log2(real(prescaler_max_c)))) downto 0);
+    signal present_state_r          : state_type;
+    -- signal present_state_r             : state_type;
+    signal bit_counter_r            : unsigned(2 downto 0);
+    signal data_counter_r           : unsigned(1 downto 0);
+    signal status_counter_r         : unsigned(3 downto 0);
+    signal param_status_r           : std_logic_vector(n_params_g - 1 downto 0);
+    signal temp_transmission_r      : temp_transmission_arr;
 
-    signal sdat_r               : std_logic;
 
-    type state_type is (start_condition, stop_condition,
-                        acknowledge, address_transfer, data_transfer);
-    signal present_state_r      : state_type;
-    signal next_state_r         : state_type;
-
-    signal bit_counter_r        : unsigned(2 downto 0);
-
-    signal status_counter_r     : unsigned(3 downto 0);
-    signal param_status_r       : std_logic_vector(n_params_g - 1 downto 0);
-
-    signal temp_address_r       : std_logic_vector(7 downto 0);
-    signal temp_data_r          : std_logic_vector(7 downto 0);
-    type transmission_arr is array (n_params_g - 1 downto 0) of std_logic_vector(15 downto 0);
-    signal transmission_c       : transmission_arr :=   (
-                                                        "0001001000000001",
-                                                        "0001000XXXXXXXXX",
-                                                        "0000111XXXXXXXXX",
-                                                        "0000110000000000",
-                                                        "0000101000000110",
-                                                        "0000100011111000",
-                                                        "0000011001111011",
-                                                        "0000010001111011",
-                                                        "0000001000011010",
-                                                        "0000000000011010"
-                                                        );
+    signal sdat_r                   : std_logic;
 
 begin
 
     sclk_out <= sclk_r;
     finished_out <= param_status_r(n_params_g - 1);
-    sdat_inout <= sdat_r;
+    sdat_r <= sdat_inout;
     param_status_out <= param_status_r;
 
     generate_sclk : process(clk, rst_n)
@@ -98,79 +97,83 @@ begin
     generate_sdat : process(clk, rst_n)
     begin
         if(rst_n = '0') then
-            sdat_r <= 'Z';
+            sdat_inout <= 'Z';
             present_state_r <= start_condition;
-            next_state_r <= start_condition;
+            present_state_r <= start_condition;
             bit_counter_r <= to_unsigned(7, bit_counter_r'length);
+            data_counter_r <= to_unsigned(0, data_counter_r'length);
             status_counter_r <= to_unsigned(0, status_counter_r'length);
             param_status_r <= (others => '0');
+            temp_transmission_r(0) <= (others => '0');
+            temp_transmission_r(1) <= (others => '0');
+            temp_transmission_r(2) <= (others => '0');
 
         elsif(clk'event and clk = '1') then
+            if(sclk_prescaler_r = prescaler_max_c / 2) then
 
-            case present_state_r is 
+                case present_state_r is 
+                    when start_condition =>
+                        if(sdat_inout = '1' and sclk_r = '1') then
+                            sdat_inout <= '0';
+                            present_state_r <= data_transfer;
+                            bit_counter_r <= to_unsigned(7, bit_counter_r'length);
+                            data_counter_r <= to_unsigned(0, data_counter_r'length);
+                            temp_transmission_r(0) <= codec_address_c;
+                            temp_transmission_r(1) <= transmission_data_c(to_integer(status_counter_r))(15 downto 8);
+                            temp_transmission_r(2) <= transmission_data_c(to_integer(status_counter_r))(7 downto 0);
+                        elsif(sclk_r = '0' and present_state_r = start_condition) then
+                            sdat_inout <= '1';
+                        end if;
 
-                when start_condition =>
-                    if(sdat_r = '1' and sclk_r = '1' and sclk_prescaler_r = prescaler_max_c / 2) then
-                        sdat_r <= '0';
-                        present_state_r <= address_transfer;
-                        temp_address_r <= transmission_c(to_integer(status_counter_r))(15 downto 8);
-                        temp_data_r <= transmission_c(to_integer(status_counter_r))(7 downto 0);
-                    elsif((sdat_r = '0' or sdat_r = 'Z') and sclk_prescaler_r = prescaler_max_c / 2) then
-                        sdat_r <= '1';
-                    end if;
-
-                when stop_condition =>
-                    if(sdat_r = '0' and sclk_r = '1' and sclk_prescaler_r = prescaler_max_c / 2) then
-                        sdat_r <= '1';
-                        param_status_r(to_integer(status_counter_r)) <= '1';
-                    elsif((sdat_r = '1' or sdat_r = 'Z') and sclk_prescaler_r = prescaler_max_c / 2) then
-                        sdat_r <= '0';
-                    elsif(sclk_prescaler_r = prescaler_max_c / 2) then
-                        sdat_r <= 'Z';
-                    end if;
-
-                when acknowledge =>
-                    if(sclk_r = '0' and sclk_prescaler_r = prescaler_max_c / 2) then
-                        if(param_status_r(n_params_g - 1) = '1') then
-                            present_state_r <= stop_condition;
+                    when stop_condition =>
+                        if(sdat_inout = '0' and sclk_r = '1') then
+                            sdat_inout <= '1';
+                            param_status_r(to_integer(status_counter_r)) <= '1';
+                        elsif((sdat_inout = '1' or sdat_inout = 'Z') and sclk_r = '0') then
+                            sdat_inout <= '0';
                         else
-                            present_state_r <= next_state_r;
-                            if(next_state_r = start_condition) then
-                                param_status_r(to_integer(status_counter_r)) <= '1';
-                                status_counter_r <= status_counter_r + 1;
+                            sdat_inout <= 'Z';
+                        end if;
+
+                    when acknowledge =>
+                        if(sclk_r = '1') then
+                            -- present_state_r <= data_transfer;
+                            -- bit_counter_r <= to_unsigned(7, bit_counter_r'length);
+                            -- data_counter_r <= data_counter_r + 1;
+                            if(sdat_inout = '1') then
+                                present_state_r <= start_condition;
+                                bit_counter_r <= to_unsigned(7, bit_counter_r'length);
+                            elsif(sdat_inout = '0') then
+                                if(status_counter_r = n_params_g - 2) then
+                                    present_state_r <= stop_condition;
+                                elsif(data_counter_r = 2) then -- to_unsigned(3, data_counter_r'length))
+                                    present_state_r <= start_condition;
+                                    bit_counter_r <= to_unsigned(7, bit_counter_r'length);
+                                    status_counter_r <= status_counter_r + 1;
+                                    param_status_r(to_integer(status_counter_r)) <= '1';
+                                else
+                                    present_state_r <= data_transfer;
+                                    bit_counter_r <= to_unsigned(7, bit_counter_r'length);
+                                    data_counter_r <= data_counter_r + 1;
+                                end if;
+                            end if;
+                        else    -- sclk_r = '0'
+                            sdat_inout <= 'Z';
+                        end if;
+
+                    when data_transfer =>
+                        if(sclk_r = '0') then
+                            sdat_inout <= temp_transmission_r(to_integer(data_counter_r))(to_integer(bit_counter_r));
+                            if(bit_counter_r = 0) then
+                                bit_counter_r <= to_unsigned(7, 3);
+                                present_state_r <= acknowledge;
+                            else
+                                bit_counter_r <= bit_counter_r - 1;
                             end if;
                         end if;
-                        sdat_r <= 'Z';
-                    end if;
 
-                when address_transfer =>
-                    if(sclk_r = '0' and sclk_prescaler_r = prescaler_max_c / 2) then
-                        sdat_r <= temp_address_r(to_integer(bit_counter_r));
-                        if(bit_counter_r = 0) then
-                            bit_counter_r <= to_unsigned(7, 3);
-                            present_state_r <= acknowledge;
-                            next_state_r <= data_transfer;
-                        else
-                            bit_counter_r <= bit_counter_r - 1;
-                        end if;
-                    end if;
-
-                when data_transfer =>
-                    if(sclk_r = '0' and sclk_prescaler_r = prescaler_max_c / 2) then
-                        sdat_r <= temp_data_r(to_integer(bit_counter_r));
-                        if(bit_counter_r = 0 and status_counter_r = n_params_g - 1) then
-                            present_state_r <= acknowledge;
-                            next_state_r <= stop_condition;
-                        elsif(bit_counter_r = 0) then
-                            bit_counter_r <= to_unsigned(7, 3);
-                            present_state_r <= acknowledge;
-                            next_state_r <= start_condition;
-                        else
-                            bit_counter_r <= bit_counter_r - 1;
-                        end if;
-                    end if;
-
-            end case;
+                end case;
+            end if;
 
         end if;
 
