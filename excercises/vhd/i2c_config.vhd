f140b51efe0724ed7f410efe9225945cc2321e14
Tuomas Huuki <tuomas.huuki@student.tut.fi>
2016-02-03 10:42:00 +0200

    Added some comments and moved the data to a more convenient place.

--- excercises/vhd/i2c_config.vhd
+++ excercises/vhd/i2c_config.vhd
@@ -16,68 +16,64 @@
 -- Revisions  :
 -- Date             Version     Author          Description
 -- 20.01.2016       1.0         nikulaj         Created
+-- 03.02.2016       1.1         huukitu         Moved data to pkg, added comments.
 -------------------------------------------------------------------------------
 library ieee;
 use ieee.std_logic_1164.all;
 use ieee.numeric_std.all;
 use ieee.math_real.all;
+use work.i2c_data_pkg.all; -- Separate package for data so that the this file does not
+                           -- have to be edited if the data is changed.
 
+-- Entity and connections.
 entity i2c_config is
     generic(
-        ref_clk_freq_g  : integer := 50000000;
-        i2c_freq_g      : integer := 20000;
-        n_params_g      : integer := 10
+        ref_clk_freq_g  : integer := 50000000;  -- Reference clock of system.
+        i2c_freq_g      : integer := 20000;     -- Clock of i2c bus.
+        n_params_g      : integer := params_g   -- Number of parameters to send.
     );
     port(
-        clk                 : in    std_logic;
-        rst_n               : in    std_logic;
-        sdat_inout          : inout std_logic;
-        sclk_out            : out   std_logic;
+        clk                 : in    std_logic;  -- Clcok
+        rst_n               : in    std_logic;  -- Reset, active low.
+        sdat_inout          : inout std_logic;  -- In/out pin of i2c data.
+        sclk_out            : out   std_logic;  -- I2c clock.
+        -- Debug helping outputs.
         param_status_out    : out   std_logic_vector(n_params_g - 1 downto 0);
         finished_out        : out   std_logic
     );
 end i2c_config;
 
 architecture rtl of i2c_config is
-
+    -- States that the controller can have.
     type state_type is (start_condition, stop_condition, acknowledge, data_transfer);
-    type transmission_data_arr is array (n_params_g - 1 downto 0) of std_logic_vector(15 downto 0);
+
     type temp_transmission_arr is array (2 downto 0) of std_logic_vector(7 downto 0);
     
     constant prescaler_max_c        : integer := (ref_clk_freq_g / i2c_freq_g) / 2;
     constant codec_address_c        : std_logic_vector(7 downto 0) := "00110100";
-    constant transmission_data_c    : transmission_data_arr := (
-                                                            "0001001000000001",
-                                                            "0001000000000010",
-                                                            "0000111000000001",
-                                                            "0000110000000000",
-                                                            "0000101000000110",
-                                                            "0000100011111000",
-                                                            "0000011001111011",
-                                                            "0000010001111011",
-                                                            "0000001000011010",
-                                                            "0000000000011010"
-                                                            );
-
-    signal sclk_r                   : std_logic;
-    signal sclk_prescaler_r         : unsigned(integer(ceil(log2(real(prescaler_max_c)))) downto 0);
-    signal present_state_r          : state_type;
-    signal bit_counter_r            : unsigned(2 downto 0);
-    signal byte_counter_r           : unsigned(1 downto 0);
-    signal status_counter_r         : unsigned(3 downto 0);
-    signal param_status_r           : std_logic_vector(n_params_g - 1 downto 0);
-    signal temp_transmission_r      : temp_transmission_arr;
+   
+    signal sclk_r                   : std_logic; -- Register for i2c clock.
+    signal sclk_prescaler_r         : unsigned(integer(ceil(log2(real(prescaler_max_c)))) downto 0); -- Clock prescaler value.
+    signal present_state_r          : state_type; -- State register.
+    signal bit_counter_r            : unsigned(2 downto 0); -- Counter for sent bits.
+    signal byte_counter_r           : unsigned(1 downto 0); -- Counter for sent bytes.
+    signal status_counter_r         : unsigned(3 downto 0); -- Counter for internal status.
+    signal param_status_r           : std_logic_vector(n_params_g - 1 downto 0); -- Debug helper.
+    signal temp_transmission_r      : temp_transmission_arr; -- Temporary transmission array.
 
 begin
-
+    
+    -- Assign debug outputs to reflect the internal status.
     finished_out <= param_status_r(n_params_g - 1);
     param_status_out <= param_status_r;
 
+    -- Set sckl to 'Z' after transmission has ended.
     with param_status_r(n_params_g - 1) select
         sclk_out <=
         sclk_r when '0',
         'Z'    when others;
 
+    -- i2c clock generation process.
     generate_sclk : process(clk, rst_n)
     begin
         if(rst_n = '0') then
@@ -93,6 +89,7 @@ begin
         end if;
     end process generate_sclk;
 
+    -- Data generation.
     generate_sdat : process(clk, rst_n)
     begin
         if(rst_n = '0') then
